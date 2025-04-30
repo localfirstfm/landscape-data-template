ce8a8efef0511870d7240a575dd29343b129bf2b
Leon Zhao <leeeon233@gmail.com>
2025-04-30 17:20:37 +0800

    feat: add loro data

--- data.js
+++ data.js
@@ -3,26 +3,37 @@ import { LandscapeSchema } from '@localfirstfm/landscape-schema'
 
 export const data = LandscapeSchema.make({
 	Version: 1,
-	Id: 'supersync',
-	Name: 'SuperSync',
-	Description: 'A cool way to sync data',
-	Website: 'https://supersync.cool',
+	Id: 'loro',
+	Name: 'Loro',
+	Logo: {
+		Light: 'https://github.com/loro-dev/loro/blob/main/docs/Loro.svg',
+		Dark: 'https://github.com/loro-dev/loro/blob/main/docs/Loro.svg',
+	},
+	Description: 'Make your JSON data collaborative and version-controlled with CRDTs',
+	MaturityLevel: 'Production-Ready',
+	Website: 'https://loro.dev',
+	GitHub: 'https://github.com/loro-dev/loro',
+	GetStarted: 'https://loro.dev/docs/tutorial/get_started',
 	Deployment: ['Self-hosted'],
 	License: 'MIT',
+	InitialReleaseDate: new Date('2023-11-13'),
 	AppTarget: {
-		LanguageSDK: { data: ['typescript'] },
+		Platform: { data: ['Browser', 'Node', 'iOS', 'macOS', 'WASM', 'Linux'] },
+		LanguageSDK: { data: ['typescript', 'javascript', 'Rust', 'Swift', 'Python'] },
+		ClientBundleSize: { data: '~900 kB' },
 	},
 	Networking: {
-		Topology: { data: 'Client-Server' },
+		Topology: { data: 'P2P' },
+		Protocol: { data: ['WebSockets', 'HTTP'] },
 	},
 	ServerSideData: {
-		PersistenceMechanism: { data: ['N/A'] },
-		DataModelParadigm: { data: 'Relational' },
+		PersistenceMechanism: { data: ['Custom'] },
+		DataModelParadigm: { data: 'Document' },
 	},
 	ClientSideData: {
 		QueryAPI: { data: ['Async'] },
-		PersistenceMechanism: { data: ['IndexedDB', 'OPFS'] },
-		PersistenceFeatures: { data: 'Indexes' },
+		LocalRefreshLatency: { data: '~1ms' },
+		PersistenceMechanism: { data: ['IndexedDB', 'Custom'] },
 		DataModel: { data: 'Document' },
 		OfflineReads: { data: 'Full Support' },
 		OfflineWrites: { data: 'Local conflict resolution' },
@@ -35,7 +46,28 @@ export const data = LandscapeSchema.make({
 		WhatGetsSynced: {
 			data: {
 				ClientToClient: 'Ops',
+				ClientToServer: 'Ops',
+				ServerToClient: 'Ops',
 			},
 		},
+		Authority: { data: 'Decentralized' },
 	},
+	UIRelated:{
+		RichTextEditing: {
+			data: 'Yes'
+		},
+		Components:{
+			data: ['ProseMirror', 'CodeMirror']
+		}
+	},
+	DevelopmentWorkflowsDX:{
+		DebuggingTools:{
+			data: ['Data Inspector'],
+			comment: "https://loro.dev/docs/advanced/inspector"
+		},
+		TypeSupport:{
+			data: 'TypeScript',
+		},
+	},
+	UserControlDataOwnership: 'Yes',
 })
